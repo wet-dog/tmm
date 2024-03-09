56f7b7a5d94ebb4208ecc92cacc06a4e6cb8627e
wet-dog <huminater@gmail.com>
2024-03-09 16:40:50 +0000

    Print kz_list

--- tmm_core.py
+++ tmm_core.py
@@ -286,6 +286,7 @@ def coh_tmm(pol, n_list, d_list, th_0, lam_vac):
     delta = kz_list * d_list
     seterr(**olderr)
 
+    print(kz_list)
     print(delta)
 
     # For a very opaque layer, reset delta to avoid divide-by-0 and similar
